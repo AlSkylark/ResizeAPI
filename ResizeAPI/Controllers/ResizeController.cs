0b6fea8cf05d04fb4fefa724ec29d0f820f3a414
Alex Castro <alxc119@gmail.com>
2021-07-03 17:52:18 +0100

    Add project files.

---
+++ ResizeAPI/Controllers/ResizeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResizeAPI.Models;
+
+namespace ResizeAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ResizeController : ControllerBase
+    {
+
+        public IActionResult Get()
+        {
+            return Ok("API is running...");
+        }
+
+        [HttpPost]
+        public IActionResult Upload([FromForm]ImageToResize ogImg)
+        {
+            if (ogImg.File.ContentType != "image/png") return Problem("Problemo");
+
+            ResizedImage image = new ResizedImage(ogImg.FileName, ogImg.File, ogImg.Size);
+            image.Resize();
+
+            
+            //for testing purposes
+            //var username = Environment.GetEnvironmentVariable("userprofile");
+            //System.IO.File.WriteAllBytes($@"{username}\desktop\test.png", image.fileBlob);
+
+
+
+            //basic log
+            float size = (float)Math.Round(ogImg.File.Length / 1048576f, 2);
+            Console.WriteLine(HttpContext.Request.HttpContext.Connection.RemoteIpAddress.ToString() + " requested a resize. File size: " + size + "mb");
+            
+            return Ok(image);
+            
+        }
+        private static async Task<string> ImageToString(IFormFile file)
+        {
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            while (reader.Peek() >= 0 )
+                {
+                    result.AppendLine(await reader.ReadLineAsync());
+                }
+            return result.ToString();
+        }
+    }
+}
---
+++ ResizeAPI/Models/ResizedImage.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResizeAPI.Models
+{
+    /// <summary>
+    /// This class contains the final thumbnail object to be send
+    /// as well as the Resize function to generate said thumbnail.
+    /// </summary>
+    public class ResizedImage
+    {
+        //JSON Object will contain these
+        public string fileName { get; set; }
+        public byte[] fileBlob { get; set; }
+        //end of JSON
+
+        private int size;
+        private IFormFile file;
+
+        /// <summary>
+        /// The constructor for this class.
+        /// </summary>
+        /// <param name="filename"> Required, the keyword 'thumb' will be prefixed.</param>
+        /// <param name="formfile"> Required, the file that was sent through the HttpRequest </param>
+        /// <param name="Size"> Optional, the size of the thumbnail to generate, default is 250 x 250px </param>
+        public ResizedImage(string filename, IFormFile formfile, int Size = 250)
+        {
+            fileName = "thumb" + filename;
+            file = formfile;
+            size = Size;
+        }
+
+        /// <summary>
+        ///This function uses the initial file passed in the constructor
+        ///and creates a thumbnail image given a specified size.
+        /// <para>Thumbnails will always be squared.</para>
+        /// </summary>
+        public void Resize()
+        {
+
+            //we get the image from the og file stream
+            Image bm = Image.FromStream(file.OpenReadStream());
+
+            //we make a thumbnail of it
+            Image thumbnail = bm.GetThumbnailImage(size, size, new Image.GetThumbnailImageAbort(ThumbnailCallback), new IntPtr());
+
+            //we pass it to a memory stream in raw format (binary array) thus creating a blob
+            //then assign the new blob to the public property
+            using (var ms = new MemoryStream())
+            {
+                thumbnail.Save(ms, ImageFormat.Png);
+                byte[] newBlob = ms.ToArray();
+                fileBlob = newBlob;
+            }
+
+
+        }
+
+        //no effing clue
+        private bool ThumbnailCallback()
+        {
+            return true;
+        }
+
+    }
+}
